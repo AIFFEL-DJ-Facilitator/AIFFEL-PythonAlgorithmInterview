39bec90534b803a686bdda7ac675b952a94d660b
mezcalagave <68863637+mezcalagave@users.noreply.github.com>
2020-12-25 13:46:23 +0900

    Add files via upload

--- ch11/p28_s1.py
+++ ch11/p28_s1.py
@@ -7,24 +7,24 @@ class ListNode:
 
 class MyHashMap:
     def __init__(self):
-        self.size = 1000
+        self.size = 1000 # 임의로 정함
         self.table = collections.defaultdict(ListNode)
 
     def put(self, key, value):
-        index = key % self.size
-        if self.table[index].value is None:
+        index = key % self.size # h(x) = x mod m
+        if self.table[index].value is None: # 해쉬 충돌이 없을 때 
             self.table[index] = ListNode(key, value)
             return
         p = self.table[index]
-        while p:
-            if p.key == key:
+        while p: 
+            if p.key == key: # 키가 (윤아 30 -> 윤아 15)
                 p.value = value
                 return
 
-            if p.next is None:
+            if p.next is None: # 끝까지 움직인 경우 
                 break
-            p = p.next
-            p.next = ListNode(key, value)
+            p = p.next 
+        p.next = ListNode(key, value) # (윤아 15 - 서현 17)
 
     def get(self, key):
         index = key % self.size
